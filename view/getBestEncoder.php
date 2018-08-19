8bc2cfd2361207cf2f1caeb4e3afe1c53597f586
daniel <daniel@daniel-DC1A-S>
2018-08-19 13:58:48 -0300

    Fix ping

--- view/getBestEncoder.php
+++ view/getBestEncoder.php
@@ -7,7 +7,7 @@ require_once $config;
 $encoders = json_decode(file_get_contents("{$global['webSiteRootURL']}view/score.php"));
 
 foreach ($encoders as $value) {
-    $ping = floatval($value->ping);
+    $ping = floatval($value->ping->value);
     $queue_size = intval($value->serverStatus->queue_size);
     $memFreeBytes = intval($value->serverStatus->memory->memFreeBytes);
     var_dump($ping, $queue_size, $memFreeBytes);
