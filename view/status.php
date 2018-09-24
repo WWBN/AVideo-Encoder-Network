3985091703961e0412ba704e22bb3944bace0b53
daniel <daniel@daniel-DC1A-S>
2018-09-24 11:35:12 -0300

    Try to add the full status

---
+++ view/status.php
@@ -0,0 +1,36 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+require_once dirname(__FILE__) . '/../configuration.php';
+require_once '../objects/Encoder.php';
+
+
+$obj = new stdClass();
+$obj->queue_size = 0;
+$obj->is_encoding = false;
+$obj->queue_list = array();
+$obj->msg = "";
+$obj->cmd = "";
+$obj->encoderSiteURL = array();
+$obj->encoding_status = "";
+$obj->encoding = array();
+$obj->download_status = array();
+
+$rows = Encoder::getAll();
+foreach ($rows as $value) {
+    $status = json_decode(file_get_contents($value['siteURL']."status"));
+    if(!empty($status)){
+        $obj->encoderSiteURL[] = $value['siteURL'];
+        $obj->queue_size[] = $status->queue_size;
+        $obj->is_encoding[] = $status->is_encoding;
+        $obj->queue_list[] = $status->queue_list;
+        $obj->msg[] = $status->msg;
+        $obj->cmd[] = $status->cmd;
+        $obj->encoding_status[] = $status->encoding_status;
+        $obj->encoding[] = $status->encoding;
+        $obj->download_status[] = $status->download_status;
+    }
+}
+
+$resp = json_encode($obj);
+echo $resp;
