291885357b7f1cbdce605bee870f3d1b0b54456f
Daniel Neto <me@danielneto.com>
2023-11-08 09:42:54 -0300

    Update

--- view/navbar.php
+++ view/navbar.php
@@ -1,16 +1,11 @@
 <!-- Top Navbar -->
 <nav class="navbar navbar-default" id="networkNavbar">
     <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <!-- Centered brand/logo -->
-            <a class="navbar-brand" href="#">
-                <img alt="Logo" src="<?php echo $streamerURL; ?>logo.png" height="20">
-            </a>
-        </div>
+        <a class="navbar-brand" href="#">
+            <img alt="Logo" src="<?php echo $streamerURL; ?>logo.png" height="20">
+        </a>
 
         <ul class="nav navbar-nav navbar-right">
-            <!-- Logoff button on the right -->
             <li><a href="<?php echo "{$global['webSiteRootURL']}logoff"; ?>" class="btn btn-default"><i class="fa-solid fa-right-from-bracket"></i> Logoff</a></li>
         </ul>
     </div><!-- /.container-fluid -->
