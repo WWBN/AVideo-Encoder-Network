05bdb036d7c4d095d00ef45d519b763cedc509c3
Daniel Neto <me@danielneto.com>
2023-11-08 09:36:25 -0300

    Update

--- view/navbar.php
+++ view/navbar.php
@@ -1,20 +1,17 @@
 <!-- Top Navbar -->
 <nav class="navbar navbar-default" id="networkNavbar">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <!-- Centered brand/logo -->
-      <a class="navbar-brand" href="#">
-        <img alt="Logo" src="<?php echo $streamerURL; ?>logo.png" height="20">
-      </a>
-    </div>
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <!-- Centered brand/logo -->
+            <a class="navbar-brand" href="#">
+                <img alt="Logo" src="<?php echo $streamerURL; ?>logo.png" height="20">
+            </a>
+        </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse">
-      <ul class="nav navbar-nav navbar-right">
-        <!-- Logoff button on the right -->
-        <li><a href="<?php echo "{$global['webSiteRootURL']}logoff"; ?>">Logoff</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+        <ul class="nav navbar-nav navbar-right">
+            <!-- Logoff button on the right -->
+            <li><a href="<?php echo "{$global['webSiteRootURL']}logoff"; ?>">Logoff</a></li>
+        </ul>
+    </div><!-- /.container-fluid -->
 </nav>
